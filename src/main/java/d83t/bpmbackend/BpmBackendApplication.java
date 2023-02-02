f0e25f6115efddf438a79b6379c2370e5a885d67
kms <aaaa727@khu.ac.kr>
2023-02-02 21:49:26 +0900

    chore: Project init

---
+++ src/main/java/d83t/bpmbackend/BpmBackendApplication.java
@@ -0,0 +1,13 @@
+package d83t.bpmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/d83t/bpmbackend/BpmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package d83t.bpmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

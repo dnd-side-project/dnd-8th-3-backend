93ccebc895202a1b649a0a46600562d2a7b198db
minseokkang <aaaa727@khu.ac.kr>
2023-02-16 22:19:01 +0900

    [D83T-16] Swagger에 맞게 응답, 요청 객체를 생성

---
+++ src/main/java/d83t/bpmbackend/domain/aggregate/profile/dto/ProfileResponse.java
@@ -0,0 +1,20 @@
+package d83t.bpmbackend.domain.aggregate.profile.dto;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Builder
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonTypeName("profile")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+@Schema(description = "카카오 로그인 API 응답 DTO")
+public class ProfileResponse {
+    private String nickname;
+    private String bio;
+
+}

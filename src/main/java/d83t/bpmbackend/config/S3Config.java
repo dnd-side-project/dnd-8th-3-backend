e8e18e9d35935e27d9ab3d67b9387e15c34f4d18
minseokkang <aaaa727@khu.ac.kr>
2023-02-23 00:50:03 +0900

    [D83T-26] S3업로드 서비스 구현

---
+++ src/main/java/d83t/bpmbackend/config/S3Config.java
@@ -0,0 +1,37 @@
+package d83t.bpmbackend.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.endpointUrl}")
+    private String endpointUrl;
+
+    @Value("${aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpointUrl, region))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
--- src/main/java/d83t/bpmbackend/exception/Error.java
+++ src/main/java/d83t/bpmbackend/exception/Error.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum Error {
     NOT_FOUND_USER_ID("kakao user not found", HttpStatus.NOT_FOUND),
-    USER_ALREADY_EXITS("user alreay exits", HttpStatus.CONFLICT),
+    USER_ALREADY_EXITS("user already exits", HttpStatus.CONFLICT),
+    S3_UPLOAD_FAIL("upload fail", HttpStatus.INTERNAL_SERVER_ERROR),
     DUPLICATE_USER_NICK_NAME("duplicate user nickname", HttpStatus.CONFLICT);
 
     private final String message;

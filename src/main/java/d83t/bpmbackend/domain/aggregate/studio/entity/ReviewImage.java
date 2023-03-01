03af16f3866c2a30b21647273523d0a62233d0bb
yesjjin99 <yesjjin99@naver.com>
2023-03-02 02:43:45 +0900

    [D83T-40] 모델 생성 오류 관련 수정

--- src/main/java/d83t/bpmbackend/domain/aggregate/studio/entity/ReviewImage.java
+++ src/main/java/d83t/bpmbackend/domain/aggregate/studio/entity/ReviewImage.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "review_image")
 public class ReviewImage extends DateEntity {
 
     @Id

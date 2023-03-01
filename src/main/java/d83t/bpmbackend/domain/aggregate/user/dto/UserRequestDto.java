1d91b8afd99ae2bfa8550823e5c2d38822824364
kms <aaaa727@khu.ac.kr>
2023-03-01 17:11:36 +0900

    [D83T-49] signUp부분 요청 dto 생성
    - 보안을 위해 쿼리스트링으로 받는것에서 폼데이터로 받도록 바꾸었습니다.

---
+++ src/main/java/d83t/bpmbackend/domain/aggregate/user/dto/UserRequestDto.java
@@ -0,0 +1,17 @@
+package d83t.bpmbackend.domain.aggregate.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "카카오 유저 확인API 요청 DTO")
+public class UserRequestDto {
+
+    @Schema(description = "kakao uid", defaultValue = "9873721L")
+    @NotBlank
+    private Long kakaoId;
+}
